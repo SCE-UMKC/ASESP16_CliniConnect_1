79e5b8f26c7e350d5dcb3b79f79cfeb098ca5fb9
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-03-06 02:06:48 -0600

    extended size of notification

--- Source/android_src/MyApplication/app/src/main/java/com/example/ase_group_1/health_app/MyGcmListenerService.java
+++ Source/android_src/MyApplication/app/src/main/java/com/example/ase_group_1/health_app/MyGcmListenerService.java
@@ -83,7 +83,8 @@ public class MyGcmListenerService extends GcmListenerService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.cliniconnect_app_size)
-                .setContentTitle("CliniConnect Appointment")
+                .setContentTitle("CliniConnect Reminder")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
